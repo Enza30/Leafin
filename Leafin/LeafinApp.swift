0a5311841892f7323078b03274728b9079287fa9
Farendza <farendza.muda@gmail.com>
2021-12-02 21:58:18 +0700

    Initial Commit

---
+++ Leafin/LeafinApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafinApp.swift
+//  Leafin
+//
+//  Created by Farendza Muda on 02/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LeafinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
